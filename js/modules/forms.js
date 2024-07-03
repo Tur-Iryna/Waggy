9ab270896d385994b6d182b1fb01fe99e747a200
Tur Iryna <115189368+Tur-Iryna@users.noreply.github.com>
2024-07-03 07:52:13 -0300

    Update forms.js

--- js/modules/forms.js
+++ js/modules/forms.js
@@ -1,16 +1,11 @@
-
-
 export const formsSubmit = () => {
   const formBox = document.querySelectorAll('[data-form]'),
     inputs = document.querySelectorAll('[data-input]');
-
   const message = {
     loading: 'Загрузка...',
     success: 'Дякуємо',
     failure: 'Щось пішло не так...'
-
   };
-
 
   const postData = async (url, data) => {
     document.querySelector('.status').textContent = message.loading;
@@ -18,28 +13,21 @@ export const formsSubmit = () => {
       method: "POST",
       body: data
     });
-
     return await result.text();
-  }
-
+  };
   const clearInput = () => {
     inputs.forEach(item => {
       item.value = "";
-    })
-  }
-
+    });
+  };
   formBox.forEach(element => {
     element.addEventListener('submit', (e) => {
       e.preventDefault();
-
-
       let messageStatus = document.createElement('div');
       messageStatus.classList.add('status');
       const modalContent = document.querySelector('.popup-cart__total');
       modalContent.innerHTML = '';
       modalContent.appendChild(messageStatus);
-
-
       const formData = new FormData(element);
       postData('server.php', formData)
         .then(result => {
@@ -47,17 +35,14 @@ export const formsSubmit = () => {
           const cartNum = document.querySelector('.cart__num');
           cartNum.innerText = 0;
           messageStatus.textContent = message.success;
-
         })
         .catch(() => messageStatus.textContent = message.failure)
         .finally(() => {
           clearInput();
           setTimeout(() => {
-
             messageStatus.remove();
           }, 3000);
-        })
-    })
-  })
-}
-
+        });
+    });
+  });
+};
