9457ff7c9d86f2954861e49365a0e5e8c1b6f83c
Tur Iryna <115189368+Tur-Iryna@users.noreply.github.com>
2024-07-03 07:52:47 -0300

    Update modals.js

--- js/modules/modals.js
+++ js/modules/modals.js
@@ -1,12 +1,8 @@
-
 const modals = () => {
   function createModals(triggerSelector, modalSelector, closeSelector) {
-
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector);
-
-
     trigger.forEach(item => {
       item.addEventListener('click', (e) => {
         if (e.target) {
@@ -22,18 +18,13 @@ const modals = () => {
       modal.style.display = "none";
       document.body.style.overflow = '';
     });
-  }
-
+  };
 
   createModals('.user-nav__profile', '.popup__user', '.popup__close');
   createModals('.user-nav__wishlist', '.popup-wishlist', '.popup-wishlist__close');
   createModals('.cart__link', '.popup-cart', '.popup-cart__close');
-
   statusProductsInWishlist();
   statusProductsInCart();
-
-}
-
-
+};
 
 export default modals;
